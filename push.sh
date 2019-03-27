5b56b4454d52008a9941f2fb26ad858e533797ff
Myrto <32554413+MyrtoP@users.noreply.github.com>
2019-03-27 16:32:34 +0200

    Update push.sh

--- push.sh
+++ push.sh
@@ -1,21 +1,20 @@
-#!/bin/sh
-
 setup_git() {
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "Travis CI"
 }
 
-commit_website_files() {
-  git checkout -b gh-pages
+commit_pdf() {
+  git checkout master
   git add cv.pdf
   git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
 }
 
 upload_files() {
-  git remote add origin-pages https://MyrtoP:${GH_TOKEN}@github.com/MyrtoP/online-cv.git 
+  git remote remove origin
+  git remote add origin https://MyrtoP:$GITHUB_TOKEN@github.com/MyrtoP/online-cv.git
   git push --quiet -u origin master
 }
 
 setup_git
-commit_website_files
+commit_pdf
 upload_files
